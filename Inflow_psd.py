07e063c306d9f5bb6782feb0267a76f80bc2a76a
StefanoWind <stefano.letizia@nrel.gov>
2024-08-22 18:27:32 -0600

    correct temporal spectrum

--- Inflow_psd.py
+++ Inflow_psd.py
@@ -32,10 +32,11 @@ edate='20230508'#end date
 
 #stats
 WD_range=[100,260]#[deg] wind direction range
-min_points_psd=144*2*2/3# minimum contigius time series length
-DT=600#resampling frequency
-max_TKE=5#[m^2/s^2]
-DT_psd=3600*2
+min_points_psd=144*2*2/3# minimum contiguous time series length
+DT=600#[s] resampling period
+max_TKE=5#[m^2/s^2] maximum TKE
+DT_psd=3600*2 #[s] resolution of spectrum
+max_T=102*3600#[s] maximum period
 
 #user-defined
 variables=['WS','WD','TKE']
@@ -55,13 +56,12 @@ from doe_dap_dl import DAP
 
 a2e = DAP('a2e.energy.gov',confirm_downloads=False)
 
-bin_f=1/np.arange(3600,7*24*3600,DT_psd)[::-1]
-
+#frequency bins
+bin_f=1/np.arange(3600,max_T+1,DT_psd)[::-1]
 f_avg=1/utl.mid(1/bin_f)
+T_avg=utl.mid(np.arange(3600,max_T+1,DT_psd))
 
 PSD=xr.Dataset()
-
-
 
 #download data
 _filter = {
@@ -72,10 +72,10 @@ _filter = {
     'file_type':'nc'
 }
 
-a2e.setup_basic_auth(username=config['username'], password=config['password'])
+# a2e.setup_basic_auth(username=config['username'], password=config['password'])
 
-utl.mkdir(os.path.join(cd,'data',channel))
-a2e.download_with_order(_filter, path=os.path.join(cd,'data',channel),replace=False)
+# utl.mkdir(os.path.join(cd,'data',channel))
+# a2e.download_with_order(_filter, path=os.path.join(cd,'data',channel),replace=False)
     
 #load log
 IN=pd.read_csv(os.path.join(cd,source_log)).replace(-9999, np.nan)
@@ -93,14 +93,14 @@ LID['WD_hh']=xr.DataArray(data=WD_int,coords={'time':LID.time})
 LID_sel=LID.where(LID['WD_hh']>WD_range[0]).where(LID['WD_hh']<WD_range[-1])
 height=LID_sel.height.values[::skip]
 
-
 for v in variables:
-    psd_avg=[]
+    psd_T=[]
     for h in height:
         print(h)
         f_sel=LID_sel[v].sel(height=h)
         if np.sum(~np.isnan(f_sel))>min_points_psd:
-        
+            
+            #find real values
             real2=np.where(np.diff(np.isnan(f_sel)+0)==1)[0]
             real1=np.where(np.diff(np.isnan(f_sel)+0)==-1)[0]+1
             
@@ -113,31 +113,37 @@ for v in variables:
             psd_all=[]
             for r1,r2 in zip(real1,real2):
                 if r2-r1>min_points_psd:
+                    
+                    #resampling
                     f=f_sel[r1:r2]
                     tnum= [utl.dt64_to_num(t) for t in f.time]
                     tnum_res=np.arange(np.min(tnum),np.max(tnum)+1,DT)
                     f_res=np.interp(tnum_res,tnum,f.values)
-                    f_psd, psd = signal.periodogram(f_res, fs=1/(DT),  scaling='density')
+                    f_psd, psd = signal.periodogram(f_res, fs=1/DT,  scaling='density')
                     
+                    #psd (frequency)
                     f_psd_all=np.append(f_psd_all,f_psd)
-                    psd_all=np.append(psd_all,psd)
+                    psd_all=np.append(psd_all,psd/np.var(f_res))
+                    
             if len(psd_all)>0:   
-                psd_avg_dim=stats.binned_statistic(f_psd_all,psd_all,statistic='mean',bins=bin_f)[0]
-                psd_avg_norm=psd_avg_dim/(np.nansum(psd_avg_dim*DT_psd/3600))
                 
-                psd_avg=utl.vstack(psd_avg,psd_avg_norm)
+                #average frequency spectrum
+                psd_avg=stats.binned_statistic(f_psd_all,psd_all,statistic='mean',bins=bin_f)[0]
+                
+                #temporal spectrum
+                psd_T=utl.vstack(psd_T,(psd_avg*f_avg**2)[::-1])
 
             else:
-                psd_avg=utl.vstack(psd_avg,f_avg*np.nan)
+                psd_T=utl.vstack(psd_T,T_avg*np.nan)
         else:
-            psd_avg=utl.vstack(psd_avg,f_avg*np.nan)
+            psd_T=utl.vstack(psd_T,T_avg*np.nan)
   
-    PSD[v]=xr.DataArray(data=psd_avg.T,coords={'frequency':f_avg,'height':height},attrs={'description':'Normalized power spectral density','units':'('+LID_sel[v].attrs['units']+')^2 h'})
+    PSD[v]=xr.DataArray(data=psd_T.T,coords={'period':T_avg,'height':height},attrs={'description':'Normalized power spectral density','units':'s^-1'})
     plt.figure()
-    plt.pcolor(1/f_avg/3600,height,np.log10(PSD[v].T))
+    plt.pcolor(T_avg/3600,height,np.log10(PSD[v].T))
     plt.xlabel(r'Period [h]')
     plt.ylabel(r'$z$ [m AGL]')
-    plt.colorbar(label='psd ['+LID_sel[v].attrs['units']+')^2 h]')
+    plt.colorbar(label='Normalized PSD [$h^{-1}$]')
     plt.grid()
     
     plt.savefig(os.path.join(cd,f'data/{WD_range[0]}-{WD_range[1]}.psd.{v}.png'))
