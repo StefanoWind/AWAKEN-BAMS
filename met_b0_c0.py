15afaa39fe1154a52285c206e9ee652db323c8a1
StefanoWind <stefano.letizia@nrel.gov>
2024-09-03 14:10:35 -0600

    added check on DA, finalized

--- met_b0_c0.py
+++ met_b0_c0.py
@@ -9,13 +9,9 @@ import sys
 import numpy as np
 import glob
 import yaml
-import pandas as pd
 import xarray as xr
 from matplotlib import pyplot as plt
 import matplotlib
-from datetime import datetime
-from scipy import signal
-from scipy import stats
 
 matplotlib.rcParams['font.family'] = 'serif'
 matplotlib.rcParams['mathtext.fontset'] = 'cm'
@@ -28,6 +24,7 @@ source_config=os.path.join(cd,'config.yaml')
 channel='awaken/sa1.met.z01.b0'
 sdate='20230724'#start date
 edate='20230801'#end date
+min_files=np.floor(24*0.8)
 
 #%% Initialization
 
@@ -63,10 +60,14 @@ utl.mkdir(os.path.join(config['path_data'],channel.replace('b0','c0')))
 dates=[utl.datestr(t,'%Y%m%d') for t in np.arange(utl.datenum(sdate,'%Y%m%d'),utl.datenum(edate,'%Y%m%d')+1,3600*24)]
 
 for d in dates:
-    Data=xr.open_mfdataset(glob.glob(os.path.join(config['path_data'],channel,'*'+d+'*nc')))
-    bin_time=np.arange(Data.time.values[0],Data.time.values[-1]+np.timedelta64(10, 'm')/2,np.timedelta64(10, 'm'))
-    Data_10m=Data.groupby_bins("time", bin_time).mean()
-    Data_10m['time_bins']=np.arange(Data.time.values[0]+np.timedelta64(10, 'm')/2,Data.time.values[-1],np.timedelta64(10, 'm'))
-    Data_10m=Data_10m.rename({"time_bins": "time"})
-    Data_10m.to_netcdf(os.path.join(config['path_data'],channel.replace('b0','c0'),channel.replace('awaken/','').replace('b0','c0')+'.'+d+'.000000.nc'))
-    print(d+' done')
+    files=glob.glob(os.path.join(config['path_data'],channel,'*'+d+'*nc'))
+    if len(files)>min_files:
+        Data=xr.open_mfdataset(files)
+        bin_time=np.arange(Data.time.values[0],Data.time.values[-1]+np.timedelta64(10, 'm')/2,np.timedelta64(10, 'm'))
+        Data_10m=Data.groupby_bins("time", bin_time).mean()
+        Data_10m['time_bins']=np.arange(Data.time.values[0]+np.timedelta64(10, 'm')/2,Data.time.values[-1],np.timedelta64(10, 'm'))
+        Data_10m=Data_10m.rename({"time_bins": "time"})
+        Data_10m.to_netcdf(os.path.join(config['path_data'],channel.replace('b0','c0'),channel.replace('awaken/','').replace('b0','c0')+'.'+d+'.000000.nc'))
+        print(d+' done')
+    else:
+        print('Not enough files on '+d)
